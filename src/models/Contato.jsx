d1c33150082e82a3fd16a3bc9f173eabf0cd79e7
PavloDev <pablogomesdg@gmail.com>
2024-05-16 22:34:30 -0300

    versão correta

--- src/models/Contato.jsx
+++ src/models/Contato.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import '../styles/Contato.css';
-
+import { FaWhatsapp } from "react-icons/fa";
 
 function Contato() {
     return (
-
         <section>
-            <div id="contato">
-                <h2>contato</h2>
+            <div className="contatoContainer">
+                <h2>Contato</h2>
 
-                <div class="orcamento">
+                <div className="orcamento">
                     <p>Quer fazer um<br />orçamento?</p>
-                    <button>solicitar orçamento</button>
+                    <button>Solicitar orçamento</button>
                 </div>
 
-                <p id="numero">21 96948-7301</p>
-                <span id="email">pabloguimadesigner@gmail.com</span>
+                <p className="numero">21 96948-7301</p>
+                <span className="email">pabloguimadesigner@gmail.com</span>
+
+                <div className='redesSociais'>
+                    <button type='button'>
+                        <FaWhatsapp />
+                    </button>
+                </div>
 
             </div>
         </section>
