ca6fc2616b1c794aacd13365194977632b2c61c9
PavloDev <pablogomesdg@gmail.com>
2024-06-06 21:43:13 -0300

    animacao floor

--- src/models/Header.jsx
+++ src/models/Header.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import '../styles/Header.css';
 import siteLogo from '../assets/images/animation.gif';
-
+import lineAnimate from '../assets/images/floor.gif';
 function Header() {
     console.log('Header renderizado');
     return (
         <div className="container">
-            <div className="image-container">
-                <img src={siteLogo} alt="andarilo" />
-            </div>
-            <div className="text-container">
-                <header>
-                    <div id="fontLogo">
-                        <h1>Andarilo</h1>
-                        <h2>studio</h2>
-                    </div>
-                </header>
+            <div className="content-wrapper">
+                <div className="image-container">
+                    <img src={siteLogo} alt="andarilo" />
+                </div>
+
+                <div className="line-animate">
+                    <img src={lineAnimate} alt="linha animada" />
+                </div>
+
+                <div className="text-container">
+                    <header>
+                        <div id="fontLogo">
+                            <h1>Andarilo</h1>
+                            <h2>studio</h2>
+                        </div>
+                    </header>
+                </div>
             </div>
             <div className="nav-container">
                 <nav>
