672e5df06c4c590d2959fa6472fc01582bbcdc64
PavloDev <pablogomesdg@gmail.com>
2024-07-02 00:22:38 -0300

    adicionando o carrossel com o react-responsive-carousel

--- src/models/Card.jsx
+++ src/models/Card.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Card.css';
 
-function Card({ fundo, titulo, page }) {
+function Card({ fundo, titulo, page, onClick }) {
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return (
         <div className="baseCard">
             <a href={page}>
-                <div className="background" style={{ backgroundImage: `url(${fundo})` }}>
+                <div className="background" onClick={handleClick} style={{ backgroundImage: `url(${fundo})` }}>
                     <p>{titulo}</p>
                 </div>
             </a>
@@ -18,6 +24,7 @@ Card.propTypes = {
     fundo: PropTypes.string.isRequired,
     titulo: PropTypes.string.isRequired,
     page: PropTypes.string.isRequired,
+    onClick: PropTypes.func, // onClick é opcional
 };
 
 export default Card;
--- src/routes/IdentidadeVisual.jsx
+++ src/routes/IdentidadeVisual.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Element } from 'react-scroll';
 import SecondHeader from '../models/SecondHeader';
 import Card from '../models/Card';
 import Projeto1 from '../assets/images/portfolio/image-nali-1.jpg';
 import '../styles/IdentidadeVisual.css';
 import Contact from '../models/Contact';
 import Footer from '../models/Footer';
-import { Element } from 'react-scroll';
+
+const imagesSet1 = [
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+];
+
+const imagesSet2 = [
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+];
+
+const imagesSet3 = [
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+];
+
+const imagesSet4 = [
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+  'https://as1.ftcdn.net/v2/jpg/00/92/53/56/1000_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
+];
+
+Modal.setAppElement('#root');
 
 function IdentidadeVisual() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImages, setCurrentImages] = useState([]);
+
+  const openModal = (images) => {
+    setCurrentImages(images);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div>
       <SecondHeader />
@@ -16,17 +56,33 @@ function IdentidadeVisual() {
         <div className='Projetos'>
           <p id='titulo'>Identidade Visual</p>
           <div className='CardsProjeto'>
-            <Card fundo={Projeto1} titulo="Identidade Visual" page="/andarilo/identidade-visual" />
-            <Card fundo={Projeto1} titulo="Identidade Visual" page="/andarilo/identidade-visual" />
-            <Card fundo={Projeto1} titulo="Identidade Visual" page="/andarilo/identidade-visual" />
-            <Card fundo={Projeto1} titulo="Identidade Visual" page="/andarilo/identidade-visual" />
+            <Card fundo={Projeto1} titulo="Identidade Visual" onClick={() => openModal(imagesSet1)} />
+            <Card fundo={Projeto1} titulo="Identidade Visual" onClick={() => openModal(imagesSet2)} />
+            <Card fundo={Projeto1} titulo="Identidade Visual" onClick={() => openModal(imagesSet3)} />
+            <Card fundo={Projeto1} titulo="Identidade Visual" onClick={() => openModal(imagesSet4)} />
           </div>
         </div>
       </div>
       <Element name="contact" className="element">
         <Contact />
       </Element>
-      < Footer />
+      <Footer />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Carousel Modal"
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <button onClick={closeModal}>Fechar</button>
+        <Carousel>
+          {currentImages.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Slide ${index}`} />
+            </div>
+          ))}
+        </Carousel>
+      </Modal>
     </div>
   );
 }
