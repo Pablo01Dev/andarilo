69da98fc43ad10b4a24903d91a8df85d621b0dc8
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-07-18 11:39:12 -0300

    Update QuemSomos.jsx

--- src/models/QuemSomos.jsx
+++ src/models/QuemSomos.jsx
@@ -17,9 +17,13 @@ function QuemSomos() {
                     <img src={Image1} alt="pablo-guimarães" />
                     <p>
                         O Andarilo é a personificação do espírito criativo e exploratório do estúdio de design, fundado por Pablo Guimarães.<br />
+                        <br/>
                         Representa a essência do constante caminhar entre ideias, desejos, caminhos e paixões que guia cada projeto.<br />
+                        <br/>
                         No Andarilho, cada criação é uma jornada única, uma oportunidade de expressar uma visão de forma técnica e artística.<br />
+                        <br/>
                         Cada projeto é tratado como uma trilha exploratória, onde cada passo é dado com intenção e paixão.<br />
+                        <br/>
                         Aqui, buscamos transmitir essa jornada não apenas como uma tarefa, mas como uma expressão autêntica de criatividade e inovação.
                     </p>
                 </div>
