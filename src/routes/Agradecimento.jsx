c92ce8797a257d72c22b2d4ae63f8eebe93830b9
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-09-06 13:34:06 -0300

    escala iframe

--- src/routes/Agradecimento.jsx
+++ src/routes/Agradecimento.jsx
@@ -4,35 +4,37 @@ import styles from '../styles/Agradecimento.module.css';
 const Agradecimento = () => {
     return (
         <div className={styles.container}>
-            <img
-                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/confete.png"
-                alt="Confete celebrando a parceria"
-                className={styles.topImage}
+            <img 
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/confete.png" 
+                alt="Confete celebrando a parceria" 
+                className={styles.topImage} 
             />
             <h1 className={styles.title}>Obrigado pela Parceria!</h1>
             <p className={styles.message}>
-                Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br />
+                Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br /> 
                 Foi um prazer desenvolver este projeto juntos!
             </p>
 
-            <div className={styles.iframeContainer}>
-                <iframe
-                    src="https://enjoydrink.com.br/"
-                    title="Emulação do site Enjoy Drink"
-                    className={styles.iframe}
-                    allowFullScreen
-                ></iframe>
-            </div>
-            <img
-                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/parceria.png"
-                alt="Parceria de sucesso"
-                className={styles.bottomImage}
+            <img 
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/parceria.png" 
+                alt="Parceria de sucesso" 
+                className={styles.bottomImage} 
             />
 
             <p className={styles.message}>
                 Esperamos continuar essa parceria e contribuir para o sucesso de vocês.<br />
                 Contem sempre conosco!
             </p>
+
+            {/* Iframe para emular o site */}
+            <div className={styles.iframeContainer}>
+                <iframe 
+                    src="https://enjoydrink.com.br/" 
+                    title="Emulação do site Enjoy Drink" 
+                    className={styles.iframe}
+                    allowFullScreen
+                ></iframe>
+            </div>
         </div>
     );
 };
