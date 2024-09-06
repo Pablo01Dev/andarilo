31fc6958eaf79c433a9ca9592872b4c593fc032f
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-09-06 13:11:05 -0300

    agradecimento pagina

--- src/routes/Agradecimento.jsx
+++ src/routes/Agradecimento.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
+import styles from '../styles/Agradecimento.module.css';
 
 const Agradecimento = () => {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Obrigado pela Parceria!</h1>
-      <p>
-        Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br/>Foi um
-        prazer desenvolver este projeto juntos!
-      </p>
-    <img src="../images/confete.png" alt="" />
+    return (
+        <div className={styles.container}>
+            <img
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/confete.png"
+                alt="Confete celebrando a parceria"
+                className={styles.topImage}
+            />
+            <h1 className={styles.title}>Obrigado pela Parceria!</h1>
+            <p className={styles.message}>
+                Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br />
+                Foi um prazer desenvolver este projeto juntos!
+            </p>
 
-      <p>
-        Esperamos continuar essa parceria e contribuir para o sucesso de vocês.
-        <br/>Contem sempre conosco!
-      </p>
-    </div>  
-  );
+            <div className={styles.iframeContainer}>
+                <iframe
+                    src="https://enjoydrink.com.br/"
+                    title="Emulação do site Enjoy Drink"
+                    className={styles.iframe}
+                    allowFullScreen
+                ></iframe>
+            </div>
+            <img
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/parceria.png"
+                alt="Parceria de sucesso"
+                className={styles.bottomImage}
+            />
+
+            <p className={styles.message}>
+                Esperamos continuar essa parceria e contribuir para o sucesso de vocês.<br />
+                Contem sempre conosco!
+            </p>
+        </div>
+    );
 };
 
 export default Agradecimento;
