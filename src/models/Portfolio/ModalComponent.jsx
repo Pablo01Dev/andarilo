8fb4a8b5e3580a23b2b9f4f7dd33548985a6c02f
PavloDev <pablogomesdg@gmail.com>
2024-09-09 11:54:33 -0300

    testando reset

--- src/models/Portfolio/ModalComponent.jsx
+++ src/models/Portfolio/ModalComponent.jsx
@@ -2,28 +2,33 @@ import React from 'react';
 import Modal from 'react-modal';
 import styles from '../../styles/ModalComponent.module.css';
 
-function ModalComponent({ isOpen, closeModal, images, title, description }) {
+function ModalComponent({ isOpen, closeModal, images, title, link, description }) {
   return (
     <div>
       <Modal
-        isOpen={isOpen}
-        onRequestClose={closeModal}
-        contentLabel="Detalhes do Projeto"
-        className={styles.modal}
-        overlayClassName={styles.Overlay}
-      >
-        <div className={styles.modalContainer}>
-          <h1>{title}</h1>
-          <div className={styles.imageContainer}>
-            {images.map((image, index) => (
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      contentLabel="Detalhes do Projeto"
+      className={styles.modal}
+      overlayClassName={styles.Overlay}
+    >
+      <div className={styles.modalContainer}>
+        <h1>{title}</h1>
+        <a href={link}>{link}</a>
+        <div className={styles.imageContainer}>
+          {images.length > 0 ? (
+            images.map((image, index) => (
               <div key={index} className={styles.imageItem}>
                 <img src={image} alt={`Slide ${index}`} />
               </div>
-            ))}
-          </div>
-          <p>{description}</p>
+            ))
+          ) : (
+            <p>No images available</p>
+          )}
         </div>
-      </Modal>
+        <p>{description}</p>
+      </div>
+    </Modal>
       {isOpen && (
         <button 
           className={styles.modalButton} 
--- src/routes/Website.jsx
+++ src/routes/Website.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Element } from 'react-scroll';
+import SecondHeader from '../models/SecondHeader';
+import CardProjetos from '../models/Portfolio/CardProjetos';
+import Contact from '../models/Contact';
+import Footer from '../models/Footer';
+import ModalComponent from '../models/Portfolio/ModalComponent';
+import { projects } from '../assets/Data/projectDataWebsite';
+
+import styles from '../styles/IdentidadeVisual.module.css';
 
 function Website() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImages, setCurrentImages] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [currentLink, setCurrentLink] = useState("");
+  const [currentDescription, setCurrentDescription] = useState("");
+
+  const openModal = (images, title, link, description) => {
+    setCurrentImages(images);
+    setCurrentTitle(title);
+    setCurrentLink(link);
+    setCurrentDescription(description);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '13em' }}>
-      <h1 style={{ margin: '2em', textAlign: 'center' }}>Página sendo atualizada</h1>
-      <Link to="/">
-        <button style={{ marginTop: '1em' }}>Voltar</button>
-      </Link>
+    <div>
+      <SecondHeader />
+      <div className={styles.conteinerProjetos}>
+        <div className={styles.projetos}>
+          <p>Identidade Visual</p>
+          <div className={styles.cardProjetos}>
+            {projects.map(project => (
+              <CardProjetos
+                key={project.id}
+                fundo={project.fundo}
+                titulo={project.title}
+                className={styles.cardProjetos}
+                descricao={project.description}
+                onClick={() => openModal(project.images, project.title, project.link, project.description)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Element name="contact" className="element">
+        <Contact />
+      </Element>
+      <Footer />
+      <ModalComponent
+        isOpen={modalIsOpen}
+        closeModal={closeModal}
+        images={currentImages}
+        link={currentLink}
+        title={currentTitle}
+        description={currentDescription}
+      />
     </div>
   );
 }
