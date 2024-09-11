f9b8ca68cd12f07ca51b5f6551969a66b46e3e52
PavloDev <pablogomesdg@gmail.com>
2024-09-11 00:47:15 -0300

    página agradecimento

--- src/routes/Agradecimento.jsx
+++ src/routes/Agradecimento.jsx
@@ -4,21 +4,32 @@ import styles from '../styles/Agradecimento.module.css';
 const Agradecimento = () => {
     return (
         <div className={styles.container}>
-            <img 
-                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/confete.png" 
-                alt="Confete celebrando a parceria" 
-                className={styles.topImage} 
+            <img
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/confete.png"
+                alt="Confete celebrando a parceria"
+                className={styles.topImage}
             />
             <h1 className={styles.title}>Obrigado pela Parceria!</h1>
             <p className={styles.message}>
-                Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br /> 
-                Foi um prazer desenvolver este projeto juntos!
+                Agradecemos imensamente pela oportunidade de trabalhar com vocês.<br />
+                Foi um prazer desenvolver o website da Enjoy!
             </p>
 
-            <img 
-                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/parceria.png" 
-                alt="Parceria de sucesso" 
-                className={styles.bottomImage} 
+            <img
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/observer.png"
+                alt="Parceria de sucesso"
+                className={styles.bottomImage}
+            />
+
+            <p className={styles.message}>
+                Vamos acompanhar o website durante 3 meses para garantir que o funcionamento não seja interrompido,
+                e durante esse período estaremos à disposição para realizar atualizações e ajustes necessário
+            </p>
+
+            <img
+                src="https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/parceria.png"
+                alt="Parceria de sucesso"
+                className={styles.bottomImage}
             />
 
             <p className={styles.message}>
@@ -26,15 +37,7 @@ const Agradecimento = () => {
                 Contem sempre conosco!
             </p>
 
-            {/* Iframe para emular o site */}
-            <div className={styles.iframeContainer}>
-                <iframe 
-                    src="https://enjoydrink.com.br/" 
-                    title="Emulação do site Enjoy Drink" 
-                    className={styles.iframe}
-                    allowFullScreen
-                ></iframe>
-            </div>
+
         </div>
     );
 };
