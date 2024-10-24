09994f3f9c4b791a63d8b2fedf51991c554597ee
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-10-24 15:58:13 -0300

    Atualizando links

--- src/assets/Data/projectDataWebsite.js
+++ src/assets/Data/projectDataWebsite.js
@@ -14,15 +14,15 @@ export const projects = [
     title: 'New Open Bar',
     fundo: 'https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/portfolio/projetos/Leo/Leo-1.jpeg',
     images: ['https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/portfolio/projetos/Nali/nali-1.jpg'],
-    link: 'https://enjoydrink.com.br/',
-    description: 'Esse foi um redesign de um logotipo de um parceiro do qual fez um excelente trabalho de marcenaria, e para retribuir a competência e cuidado com trabalho que foi entregue, resolvi presenteá-lo com esse redesign.',
+    link: 'https://newopenbar.com.br/',
+    description: '',
   },
   {
     id: 3,
     title: 'Rabello Ammim',
     fundo: 'https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/portfolio/projetos/MR/mr-2.jpg',
     images: ['https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/portfolio/projetos/Nali/nali-1.jpg'],
-    link: 'https://enjoydrink.com.br/',
+    link: 'https://ceirabelloamim.com.br',
     description: '',
   },
 
