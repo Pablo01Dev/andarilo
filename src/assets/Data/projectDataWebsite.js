e51666a5342e0d7c0ea6b9d075e0a3a4f08c0d88
PavloDev <pablogomesdg@gmail.com>
2024-10-24 19:29:55 -0300

    ajustando extensão do link

--- src/assets/Data/projectDataWebsite.js
+++ src/assets/Data/projectDataWebsite.js
@@ -12,7 +12,7 @@ export const projects = [
   {
     id: 2,
     title: 'New Open Bar',
-    fundo: 'https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/new-open.jpeg',
+    fundo: 'https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/new-open.jpg',
     images: ['https://raw.githubusercontent.com/Pablo01Dev/andarilo/main/src/assets/images/portfolio/projetos/Nali/nali-1.jpg'],
     link: 'https://newopenbar.com.br/',
     description: '',
